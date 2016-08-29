c7d06adffcdde9eb7857680838dc0f0720357f80
scottlaw1 <slawrence@gmail.com>
2016-08-29 16:39:23 -0400

    Completed lesson 1.

--- src/Unit-1/DoThis/ConsoleWriterActor.cs
+++ src/Unit-1/DoThis/ConsoleWriterActor.cs
@@ -22,11 +22,11 @@ namespace WinTail
                 return;
             }
 
-            // if message has even # characters, display in red; else, green
             var even = msg.Length % 2 == 0;
-            var color = even ? ConsoleColor.Red : ConsoleColor.Green;
             var alert = even ? "Your string had an even # of characters.\n" : "Your string had an odd # of characters.\n";
-            Console.ForegroundColor = color;
+
+            // if message has even # characters, display in red; else, green
+            Console.ForegroundColor = even ? ConsoleColor.Red : ConsoleColor.Green; ;
             Console.WriteLine(alert);
             Console.ResetColor();
 
